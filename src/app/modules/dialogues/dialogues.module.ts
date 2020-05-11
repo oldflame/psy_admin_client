4dfd7cfc9ad644f32e22d8bcd61866175c8faa71
Dhaval Dedhia <dhavl32@gmail.com>
2020-05-10 22:16:32 -0400

    Added UI for modal qindow for adding questions category

--- src/app/modules/dialogues/dialogues.module.ts
+++ src/app/modules/dialogues/dialogues.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddQuestionComponent } from './add-question/add-question.component';
 import { AddQuestionCategoryComponent } from './add-question-category/add-question-category.component';
+import { MaterialModule } from 'src/app/material.module';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [AddQuestionComponent, AddQuestionCategoryComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    FormControl
   ]
 })
 export class DialoguesModule { }
--- src/app/modules/question-management/categories/categories.component.ts
+++ src/app/modules/question-management/categories/categories.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { AddQuestionComponent } from "../../dialogues/add-question/add-question.component";
+import { switchMap } from "rxjs/operators";
+import { QuestionCategory } from "src/app/models/question-category";
+import { EMPTY } from "rxjs";
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
-  selector: 'categories',
-  templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss']
+  selector: "categories",
+  templateUrl: "./categories.component.html",
+  styleUrls: ["./categories.component.scss"],
 })
 export class CategoriesComponent implements OnInit {
+  dialogRef;
 
-  constructor() { }
+  constructor(private dialog: MatDialog, private categoryService: CategoryService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  showAddCategoryDialog() {
+    this.dialogRef = this.dialog.open(AddQuestionComponent, {
+      width: "800px",
+      closeOnNavigation: true,
+    });
+
+    this.dialogRef
+      .afterClosed()
+      .pipe(
+        switchMap((res: any) => {
+          if (res) {
+            return this.categoryService.addQuestionCategory(res);
+          }
+          return EMPTY;
+        })
+      )
+      .subscribe();
   }
-
 }
