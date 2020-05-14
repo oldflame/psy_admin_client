120536ab7db69d271a10304cc9b8e3bc1cd81829
Jainam Sheth <j4inam@gmail.com>
2020-05-14 00:09:49 -0400

    Added list component for images

--- src/app/modules/general/lists/lists.module.ts
+++ src/app/modules/general/lists/lists.module.ts
@@ -1,17 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LocationListComponent } from './location-list/location-list.component';
-import { MaterialModule } from '../../../material.module';
-import { CategoriesListComponent } from './categories-list/categories-list.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { LocationListComponent } from "./location-list/location-list.component";
+import { MaterialModule } from "../../../material.module";
+import { CategoriesListComponent } from "./categories-list/categories-list.component";
+import { ImagesListComponent } from "./images-list/images-list.component";
+import { MomentModule } from 'ngx-moment';
 
 @NgModule({
-  declarations: [LocationListComponent, CategoriesListComponent],
-  imports: [
-    CommonModule,
-    MaterialModule
+  declarations: [
+    LocationListComponent,
+    CategoriesListComponent,
+    ImagesListComponent,
   ],
-  exports: [LocationListComponent, CategoriesListComponent]
+  imports: [CommonModule, MaterialModule, MomentModule],
+  exports: [
+    LocationListComponent,
+    CategoriesListComponent,
+    ImagesListComponent,
+  ],
 })
-export class ListsModule { }
+export class ListsModule {}
