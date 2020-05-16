55d85942bdc1c1cc33e123d5b178cac9ab382206
Jainam Sheth <j4inam@gmail.com>
2020-05-15 21:21:01 -0400

    Merge fixes in questionnaire component

--- src/app/modules/question-management/questions/questions.component.ts
+++ src/app/modules/question-management/questions/questions.component.ts
@@ -5,12 +5,12 @@ import { switchMap, map } from "rxjs/operators";
 import { EMPTY, Observable } from "rxjs";
 import { QuestionsService } from "src/app/services/questions.service";
 import { HttpErrorResponse } from "@angular/common/http";
-import { CategoryService } from 'src/app/services/category.service';
-import { QuestionCategory } from 'src/app/models/question-category';
-import { ActionConfirmDialogComponent } from '../../general/dialogs/action-confirm-dialog/action-confirm-dialog.component';
-import { ToastService, TOAST_TYPE } from 'src/app/services/toast.service';
-import { Question } from 'src/app/models/question';
-import { FormControl } from '@angular/forms';
+import { CategoryService } from "src/app/services/category.service";
+import { QuestionCategory } from "src/app/models/question-category";
+import { ActionConfirmDialogComponent } from "../../general/dialogs/action-confirm-dialog/action-confirm-dialog.component";
+import { ToastService, TOAST_TYPE } from "src/app/services/toast.service";
+import { Question } from "src/app/models/question";
+import { FormControl } from "@angular/forms";
 import * as _ from "lodash";
 
 @Component({
@@ -31,12 +31,14 @@ export class QuestionsComponent implements OnInit {
     private toastService: ToastService
   ) {}
 
-  ngOnInit(): void {  
+  ngOnInit(): void {
     this.questions$ = this.questionService.questions$.pipe(
       map((questionCategories: Question[]) => {
         if (questionCategories && questionCategories.length > 0) {
           if (!this.showAllQuestions) {
-            questionCategories = questionCategories.filter(question => !question.isDeleted)
+            questionCategories = questionCategories.filter(
+              (question) => !question.isDeleted
+            );
           }
           questionCategories = _.sortBy(questionCategories, "name");
         }
@@ -53,7 +55,7 @@ export class QuestionsComponent implements OnInit {
     this.deletedQuestionsToggle.valueChanges.subscribe((value) => {
       this.showAllQuestions = value;
       this.questionService.rebroadcastCategoriesData();
-    })
+    });
   }
 
   searchTextChanged(eventArgs) {
@@ -122,8 +124,9 @@ export class QuestionsComponent implements OnInit {
           }
         },
         (err: HttpErrorResponse) => {
-          this.toastService.showToast(err.error.msg, TOAST_TYPE.DANGER)
-      });
+          this.toastService.showToast(err.error.msg, TOAST_TYPE.DANGER);
+        }
+      );
   }
 
   restoreQuestion(eventArgs: any) {
@@ -150,19 +153,23 @@ export class QuestionsComponent implements OnInit {
           return EMPTY;
         })
       )
-      .subscribe((serverRes: boolean) => {
-        if (serverRes) {
-          this.toastService.showToast(
-            "Question restored Successfully!",
-            TOAST_TYPE.SUCCESS
-          );
-        } else {
-          this.toastService.showToast(
-            "Failed to restore Question. Try again!",
-            TOAST_TYPE.DANGER
-          );
-        },(err: HttpErrorResponse) => {
-          this.toastService.showToast(err.error.msg, TOAST_TYPE.DANGER)
-      });
+      .subscribe(
+        (serverRes: boolean) => {
+          if (serverRes) {
+            this.toastService.showToast(
+              "Question restored Successfully!",
+              TOAST_TYPE.SUCCESS
+            );
+          } else {
+            this.toastService.showToast(
+              "Failed to restore Question. Try again!",
+              TOAST_TYPE.DANGER
+            );
+          }
+        },
+        (err: HttpErrorResponse) => {
+          this.toastService.showToast(err.error.msg, TOAST_TYPE.DANGER);
+        }
+      );
   }
 }
